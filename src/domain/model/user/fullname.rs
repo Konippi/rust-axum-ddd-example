650120574c0f281f1791fbeb041b56ed306b776d
Konippi <konippi-114@outlook.jp>
2024-06-29 23:42:12 +0900

    feat: add value object for full name

--- src/domain/model/user/fullname.rs
+++ src/domain/model/user/fullname.rs
@@ -1,20 +1,39 @@
-#[derive(Clone, Debug, PartialEq, Eq)]
+use std::str::FromStr;
+
+use derive_getters::Getters;
+use regex::Regex;
+
+#[derive(Clone, Debug, Getters, PartialEq, Eq)]
 pub struct FullName {
-    pub first_name: String,
-    pub last_name: String,
+    first_name: Name,
+    last_name: Name,
 }
 
 impl FullName {
-    pub fn new(first_name: &str, last_name: &str) -> Self {
-        Self {
-            first_name: first_name.trim().to_string(),
-            last_name: last_name.trim().to_string(),
+    pub fn new(first_name: &String, last_name: &String) -> anyhow::Result<Self> {
+        Ok(Self {
+            first_name: first_name.parse()?,
+            last_name: last_name.parse()?,
+        })
+    }
+}
+
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Name(String);
+
+impl FromStr for Name {
+    type Err = String;
+    fn from_str(name: &String) -> Result<Self, Self::Error> {
+        let trimmed_name = name.trim();
+        if trimmed_name.is_empty() {
+            anyhow::bail!("At least one character.")
         }
-    }
-    pub fn first_name(&self) -> String {
-        self.first_name.clone()
-    }
-    pub fn last_name(&self) -> String {
-        self.last_name.clone()
+
+        let regex = Regex::new(r#"^[a-zA-Z]+$"#).unwrap();
+        if !regex.is_match(name.trim()) {
+            anyhow::bail!("Non-alphabetic characters are not allowed.")
+        }
+        Ok(Name(trimmed_name.to_string()))
     }
 }
